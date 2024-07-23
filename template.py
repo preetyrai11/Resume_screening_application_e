5a2fc81d637767dffe6ee2d78bb45e8e6ec8185f
preetyrai11 <drpreetyrai@gmail.com>
2024-07-23 17:53:56 +0530

    added template.py file

---
+++ template.py
@@ -0,0 +1,37 @@
+import os 
+from pathlib import Path 
+import logging 
+
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s') 
+
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/{project_name}/utils/__init__.py",
+    "src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    ".env",
+    "requirements.txt",
+    "setup.py",
+    "app.py",
+    "research/trails.ipynb",
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
