ee813d19f0a68ac5746375d68efbb50912e4039a
preetyrai11 <drpreetyrai@gmail.com>
2024-07-23 18:45:10 +0530

    files added

--- README.md
+++ README.md
@@ -11,5 +11,14 @@ FAISS --> Vector DB
 
 
 version -> python 3.8.18 
+ 
+google bard 
 
+
+# create a virtual environment
+conda create -n genai python=3.8 -y
+
+
+
+in .ipynb also it works on python 3.8.18 
 
--- template.py
+++ template.py
@@ -9,9 +9,6 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
 list_of_files = [
     "src/__init__.py",
     "src/helper.py",
-    "src/{project_name}/utils/__init__.py",
-    "src/{project_name}/constants/__init__.py",
-    "config/config.yaml",
     ".env",
     "requirements.txt",
     "setup.py",
@@ -21,6 +18,23 @@ list_of_files = [
 
 
 
+for filepath in list_of_files:
+    filepaht = Path(filepath) 
+    filedir, filename = os.path.split(filepath) 
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True) 
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")  
+
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0): 
+        with open(filename, "w") as f:
+            pass 
+            logging.info(f"Creating empty file: {filepath}") 
+    
+    else:
+        logging.info(f"{filename} is already exists") 
+    
 
 
 
